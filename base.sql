c8ace8dd10931ce3e2a379bf9d8fe5aac9012076
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-19 09:31:04 +0100

    derniere resolution potentielle..

--- base.sql
+++ base.sql
@@ -15,15 +15,12 @@ ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-DD-MM HH24:MI';
 -- ============================================================
 
 
-drop table COMMUNES cascade constraints;
-drop table PARKINGS cascade constraints;
-drop table PLACES cascade constraints;
 drop table STATIONNEMENTS cascade constraints;
+drop table PLACES cascade constraints;
+drop table PARKINGS cascade constraints;
+drop table COMMUNES cascade constraints;
 drop table VEHICULES cascade constraints;
 
-drop sequence NUMERO_PARKING_seq;
-drop sequence NUMERO_PLACE_seq;
-drop sequence ID_STATIONNEMENT_seq;
 
 -- ============================================================
 -- ============================================================
@@ -37,33 +34,27 @@ drop sequence ID_STATIONNEMENT_seq;
 
 create table COMMUNES
 (
-CODE_POSTAL	NUMBER(5)	not null,
-NOM_COMMUNE	CHAR(20)	not null,
-constraint pk_commune primary key (CODE_POSTAL)
+	CODE_POSTAL	NUMBER(5)	not null,
+	NOM_COMMUNE	CHAR(20)	not null,
+	constraint pk_commune primary key (CODE_POSTAL)
 );
 
 -- ============================================================
 --   Table : PARKINGS
 -- ============================================================
 
-create sequence NUMERO_PARKING_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
 
 create table PARKINGS
 (
-NUMERO_PARKING	NUMBER(3)	not null,
-NOM_PARKING 	CHAR(30)	not null,
-ADRESSE		CHAR(40)	not null,
-TARIF_HORAIRE 	NUMBER(4)	not null,
-CAPACITE 	NUMBER(5)	not null,
-CODE_POSTAL	NUMBER(5)	not null,
-constraint pk_parking primary key (NUMERO_PARKING,CODE_POSTAL),
-check(CAPACITE >= 1),
-check(TARIF_HORAIRE >= 1),
+	NUMERO_PARKING	NUMBER(3)	not null,
+	NOM_PARKING 	CHAR(30)	not null,
+	ADRESSE		CHAR(40)	not null,
+	TARIF_HORAIRE 	NUMBER(4)	not null,
+	CAPACITE	 	NUMBER(5)	not null,
+	CODE_POSTAL	NUMBER(5)	not null,
+	constraint pk_parking primary key (NUMERO_PARKING,CODE_POSTAL),
+	check(CAPACITE >= 1),
+	check(TARIF_HORAIRE >= 1),
 );
 
 
@@ -71,43 +62,30 @@ check(TARIF_HORAIRE >= 1),
 --   Table : PLACES
 -- ============================================================
 
-create sequence NUMERO_PLACE_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
 
 create table PLACES
 (
-NUMERO_PLACE	NUMBER(5)	not null,
-NOM_PLACE	NUMBER(10)	not null,
+	NUMERO_PLACE	NUMBER(5)	not null,
+	NOM_PLACE	NUMBER(10)	not null,
 --chaque numero correspond a une signification particuliere de la place
 --d'ou un nom attribue(on procede de cette maniere).
-NUMERO_PARKING	NUMBER(3) 	not null,	  
-constraint pk_place primary key (NUMERO_PLACE,NUMERO_PARKING)
+       NUMERO_PARKING	NUMBER(3) 	not null,	  
+       constraint pk_place primary key (NUMERO_PLACE,NUMERO_PARKING)
 );
 
 -- ============================================================
 --   Table : STATIONNEMENTS
 -- ============================================================
 
-create sequence ID_STATIONNEMENT_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
-
 
 create table STATIONNEMENTS
 (
-ID_STATIONNEMENT	NUMBER(4)	not null,
-DATE_STATIONNEMENT 	DATE		not null,
-HORAIRE_SORTIE		DATE		not null,
-NUMERO_PLACE		NUMBER(5)	not null,
-NUMERO_IMMATRICULATION	CHAR(11)	not null,
-constraint pk_stationnement primary key (ID_STATIONNEMENT,NUMERO_PLACE,NUMERO_IMMATRICULATION)
+	ID_STATIONNEMENT	NUMBER(4)	not null,
+	DATE_STATIONNEMENT 	DATE		not null,
+	HORAIRE_SORTIE		DATE		not null,
+	NUMERO_PLACE		NUMBER(5)	not null,
+	NUMERO_IMMATRICULATION	CHAR(11)	not null,
+	constraint pk_stationnement primary key (ID_STATIONNEMENT,NUMERO_PLACE,NUMERO_IMMATRICULATION)
 );
 
 -- ============================================================
@@ -116,13 +94,13 @@ constraint pk_stationnement primary key (ID_STATIONNEMENT,NUMERO_PLACE,NUMERO_IM
 
 create table VEHICULES
 (
-NUMERO_IMMATRICULATION	CHAR(11)	not null,
-MARQUE 			CHAR(17)	not null,
-DATE_DE_MISE_EN_CIRCULATION		DATE	not null,
-KILOMETRAGE 				NUMBER(7)   not null,
-ETAT					CHAR(8)	    not null,
-constraint pk_vehicule primary key (NUMERO_IMMATRICULATION),
-check(KILOMETRAGE >= 1),
+	NUMERO_IMMATRICULATION	CHAR(11)	not null,
+	MARQUE 			CHAR(17)	not null,
+	DATE_DE_MISE_EN_CIRCULATION		DATE	not null,
+	KILOMETRAGE 				NUMBER(7)   not null,
+	ETAT					CHAR(8)	    not null,
+	constraint pk_vehicule primary key (NUMERO_IMMATRICULATION),
+	check(KILOMETRAGE >= 1),
 );
 
 -- ============================================================
