ef2254d90f4c9510c30c8e8c633c947e6d9bad4b
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-19 03:01:35 +0100

    ajout d'un script affichage (ENTIER)

---
+++ affichage.sql
@@ -0,0 +1,22 @@
+-- ============================================================
+--   Nom de la base   :  Parking_campus
+--   Nom du fichier   :  affichage.sql
+--   Nom de SGBD      :  ORACLE Version 7.0
+--   Date de creation :  19/11/2022 03:00
+-- ============================================================
+
+-- ============================================================
+-- !!!!!!  VOUS DEVEZ EXECUTER CES FICHIERS AVANT:
+-- !!!!!!           - base.sql
+-- !!!!!!           - donnees.sql
+-- ============================================================
+
+select * from COMMUNES;
+
+select * from PARKINGS;
+
+select * from PLACES;
+
+select * from STATIONNEMENTS;
+
+select * from VEHICULES;
