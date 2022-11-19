666d3df65532f5b526c29dca5a38eb3820cc1eda
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-19 03:19:20 +0100

    correction(3)..

--- base.sql
+++ base.sql
@@ -130,17 +130,17 @@ check(KILOMETRAGE >= 1),
 
 alter table PARKINGS
       add constraint fk1_code_postal FOREIGN KEY(CODE_POSTAL)
-      references COMMUNES(CODE_POSTAL)
+      references COMMUNES(CODE_POSTAL);
 
 alter table PLACES
       add constaint fk1_numero_parking FOREIGN KEY(NUMERO_PARKING)
-      references PARKINGS(NUMERO_PARKING)
+      references PARKINGS(NUMERO_PARKING);
 
 alter table STATIONNEMENTS
       add constraint fk1_numero_place FOREIGN KEY(NUMERO_PLACE)
-      references PLACES(NUMERO_PLACE)
+      references PLACES(NUMERO_PLACE);
 alter table STATIONNEMENTS
       add constraint fk2_numero_immatriculation FOREIGN KEY(NUMERO_IMMATRICULATION)
-      references VEHICULES(NUMERO_IMMATRICULATION)
+      references VEHICULES(NUMERO_IMMATRICULATION);
 
 commit;
