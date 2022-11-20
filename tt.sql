c34b543a7e7241dbbda2a058add7e4bb68a1f013
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-20 02:09:45 +0100

    test..

--- tt.sql
+++ tt.sql
@@ -4,13 +4,15 @@
 --   Date de creation :  30/10/96  12:09                       
 -- ============================================================
 
-drop table ROLE cascade constraints;
+drop table STATIONNEMENTS cascade constraints;
 
-drop table FILM cascade constraints;
+drop table PLACES cascade constraints;
 
-drop table REALISATEUR cascade constraints;
+drop table PARKINGS cascade constraints;
 
-drop table ACTEUR cascade constraints;
+drop table COMMUNES cascade constraints;
+
+drop table VEHICULES cascade constraints;
 
 -- ============================================================
 --   Table : ACTEUR                                            
@@ -25,6 +27,14 @@ create table ACTEUR
     constraint pk_acteur primary key (NUMERO_ACTEUR)
 );
 
+create table COMMUNES
+(
+	CODE_POSTAL	NUMBER(5)	not null,
+	NOM_COMMUNE	CHAR(20)		,
+	constraint pk_commune primary key (CODE_POSTAL)
+);
+
+
 -- ============================================================
 --   Table : REALISATEUR                                       
 -- ============================================================
@@ -35,6 +45,16 @@ create table REALISATEUR
     PRENOM_REALISATEUR              CHAR(20)                       ,
     NATION_REALISATEUR              CHAR(20)                       ,
     constraint pk_realisateur primary key (NUMERO_REALISATEUR)
+);
+
+create table VEHICULES
+(
+	NUMERO_IMMATRICULATION	CHAR(11)	not null,
+	MARQUE 			CHAR(17)	not null,
+	DATE_DE_MISE_EN_CIRCULATION		DATE	not null,
+	KILOMETRAGE 				NUMBER(7)   not null,
+	ETAT					CHAR(8)	    ,
+	constraint pk_vehicule primary key (NUMERO_IMMATRICULATION)
 );
 
 -- ============================================================
@@ -51,6 +71,18 @@ create table FILM
     constraint pk_film primary key (NUMERO_FILM)
 );
 
+create table PARKINGS
+(
+	NUMERO_PARKING	NUMBER(3)	not null,
+	NOM_PARKING 	CHAR(30)	not null,
+	ADRESSE		CHAR(40)	not null,
+	TARIF_HORAIRE 	NUMBER(4)	not null,
+	CAPACITE	NUMBER(5)	,
+	CODE_POSTAL	NUMBER(5)	not null,
+	constraint pk_parking primary key (NUMERO_PARKING)
+);
+
+
 -- ============================================================
 --   Table : ROLE                                              
 -- ============================================================
@@ -62,9 +94,28 @@ create table ROLE
     constraint pk_role primary key (NUMERO_ACTEUR, NUMERO_FILM)
 );
 
+create table STATIONNEMENTS
+(
+	ID_STATIONNEMENT	NUMBER(4)	not null,
+	DATE_STATIONNEMENT 	DATE		not null,
+	HORAIRE_SORTIE		DATE		not null,
+	NUMERO_PLACE		NUMBER(5)	not null,
+	NUMERO_IMMATRICULATION	CHAR(11)	not null,
+	constraint pk_stationnement primary key (ID_STATIONNEMENT)
+);
+
+
 alter table FILM
     add constraint fk1_film foreign key (NUMERO_REALISATEUR)
        references REALISATEUR (NUMERO_REALISATEUR);
+
+alter table PARKINGS
+      add constraint fk1_parking foreign key (CODE_POSTAL)
+      	  references COMMUNES (CODE_POSTAL);
+
+alter table PLACES
+      add constraint fk1_place foreign key (NUMERO_PARKING)
+      	  references PARKINGS (NUMERO_PARKING);
 
 alter table ROLE
     add constraint fk1_role foreign key (NUMERO_ACTEUR)
@@ -74,3 +125,10 @@ alter table ROLE
     add constraint fk2_role foreign key (NUMERO_FILM)
        references FILM (NUMERO_FILM);
 
+alter table STATIONNEMENTS
+      add constraint fk1_stationnement foreign key (NUMERO_PLACE)
+      	  references PLACES (NUMERO_PLACE);
+	  
+alter table STATIONNEMENTS
+      add constraint fk2_stationnement foreign key (NUMERO_IMMATRICULATION)
+      	  references VEHICULES (NUMERO_IMMATRICULATION);
