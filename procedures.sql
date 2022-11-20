aa83c6c938542acf40880b35da1808eb85ce0812
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-20 12:22:52 +0100

    version 4.1.1

---
+++ procedures.sql
@@ -0,0 +1,31 @@
+-- ============================================================
+--   Nom de la base   :  Parking_campus
+--   Nom du fichier   :  procedures.sql
+--   Nom de SGBD      :  ORACLE Version 7.0
+--   Date de creation :  20/11/2022 12:13
+-- ============================================================
+
+-- ============================================================
+-- !!!!!! VOUS DEVEZ EXECUTER CES FICHIERS AVANT:
+-- !!!!!!           - base.sql
+-- !!!!!            - vues.sql
+-- ============================================================
+
+--fonction dbms_output.put_line.
+--operateur || de concatenation de chaines.
+
+--1ere procedure:
+--Un numero de place qui participe pas à un stationnement
+
+create or replace procedure procedure_places_stats
+       is
+	begin
+	for compteur in (select * from vue_places_stats)
+	loop
+		if compteur.id_stationnement is not null then
+		   dbms_output.put_line('Cette place ne concerne aucun stationnement jusquà maintenant : ||', compteur.NOM_POSITION);
+		end if;
+	end loop;
+	end;
+/
+		   
