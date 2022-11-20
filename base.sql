36d21caa968b9a55af9af5731a59b2534400b212
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-20 02:51:55 +0100

    base.sql operationnel, voyons si on peut charger dataat database..

--- base.sql
+++ base.sql
@@ -1,35 +1,19 @@
 -- ============================================================
---   Nom de la base   :  Parking_campus
---   Nom du fichier   :  base.sql
---   Nom de SGBD      :  ORACLE Version 7.0
---   Date de creation :  19/11/2022  01:43
+--   Nom de la base   :  CINEMA                                
+--   Nom de SGBD      :  ORACLE Version 7.0                    
+--   Date de creation :  30/10/96  12:09                       
 -- ============================================================
-
---Format des dates de la session
-ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-DD-MM HH24:MI';
-
--- ============================================================
--- ============================================================
---                           DROP
--- ============================================================
--- ============================================================
-
 
 drop table STATIONNEMENTS cascade constraints;
-drop table PLACES cascade constraints;
+
+drop table POSITIONS cascade constraints;
+
 drop table PARKINGS cascade constraints;
-drop table STATIONNEMENTS cascade constraints;
+
 drop table VEHICULES cascade constraints;
 
-drop sequence NUMERO_PARKING_seq;
-drop sequence NUMERO_PLACE_seq;
-drop sequence ID_STATIONNEMENT_seq;
+drop table COMMUNES cascade constraints;
 
--- ============================================================
--- ============================================================
---                   CREATE TABLE/CREATE SEQUENCE
--- ============================================================
--- ============================================================
 
 -- ============================================================
 --   Table : COMMUNES
@@ -38,20 +22,29 @@ drop sequence ID_STATIONNEMENT_seq;
 create table COMMUNES
 (
 	CODE_POSTAL	NUMBER(5)	not null,
-	NOM_COMMUNE	CHAR(20)	not null,
+	NOM_COMMUNE	CHAR(20)		,
 	constraint pk_commune primary key (CODE_POSTAL)
+);
+
+
+-- ============================================================
+--   Table : VEHICULES                                       
+-- ============================================================
+
+create table VEHICULES
+(
+	NUMERO_IMMATRICULATION	CHAR(11)	not null,
+	MARQUE 			CHAR(17)	not null,
+	DATE_DE_MISE_EN_CIRCULATION		DATE	not null,
+	TYPE_VEHICULE				CHAR(25)    ,
+	KILOMETRAGE 				NUMBER(7)   not null,
+	ETAT					CHAR(8)	    ,
+	constraint pk_vehicule primary key (NUMERO_IMMATRICULATION)
 );
 
 -- ============================================================
 --   Table : PARKINGS
 -- ============================================================
-
-create sequence NUMERO_PARKING_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
 
 create table PARKINGS
 (
@@ -59,46 +52,29 @@ create table PARKINGS
 	NOM_PARKING 	CHAR(30)	not null,
 	ADRESSE		CHAR(40)	not null,
 	TARIF_HORAIRE 	NUMBER(4)	not null,
-	CAPACITE	NUMBER(5)	not null,
+	CAPACITE	NUMBER(5)	,
 	CODE_POSTAL	NUMBER(5)	not null,
-	constraint pk_parking primary key (NUMERO_PARKING,CODE_POSTAL),
-	check(CAPACITE >= 1),
-	check(TARIF_HORAIRE >= 1),
+	constraint pk_parking primary key (NUMERO_PARKING)
 );
 
 
 -- ============================================================
---   Table : PLACES
+--   Table : POSITIONS          
 -- ============================================================
 
-create sequence NUMERO_PLACE_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
-
-create table PLACES
+create table POSITIONS
 (
-	NUMERO_PLACE	NUMBER(5)	not null,
-	NOM_PLACE	NUMBER(10)	not null,
---chaque numero correspond a une signification particuliere de la place
---d'ou un nom attribue(on procede de cette maniere).
-       NUMERO_PARKING	NUMBER(3) 	not null,	  
-       constraint pk_place primary key (NUMERO_PLACE,NUMERO_PARKING)
+	NUMERO_PLACE NUMBER(4)	not null,
+	NOM_PLACE    CHAR(20)		,
+	NOM_POSITION CHAR(30)		,
+	NUMERO_PARKING			NUMBER(3)	not null,
+	constraint pk_position primary key (NUMERO_PLACE)
 );
+	
 
 -- ============================================================
---   Table : STATIONNEMENTS
+--   Table : STATIONNEMENTS                                          
 -- ============================================================
-
-create sequence ID_STATIONNEMENT_seq
-       minvalue 1
-       maxvalue 999
-       start with 1
-       increment by 1
-       cache 10;
-
 
 create table STATIONNEMENTS
 (
@@ -107,44 +83,26 @@ create table STATIONNEMENTS
 	HORAIRE_SORTIE		DATE		not null,
 	NUMERO_PLACE		NUMBER(5)	not null,
 	NUMERO_IMMATRICULATION	CHAR(11)	not null,
-	constraint pk_stationnement primary key (ID_STATIONNEMENT,NUMERO_PLACE,NUMERO_IMMATRICULATION)
+	constraint pk_stationnement primary key (ID_STATIONNEMENT)
 );
 
--- ============================================================
---   Table : VEHICULES
--- ============================================================
 
-create table VEHICULES
-(
-	NUMERO_IMMATRICULATION	CHAR(11)	not null,
-	MARQUE 			CHAR(17)	not null,
-	DATE_DE_MISE_EN_CIRCULATION		DATE	not null,
-	KILOMETRAGE 				NUMBER(7)   not null,
-	ETAT					CHAR(8)	    not null,
-	constraint pk_vehicule primary key (NUMERO_IMMATRICULATION),
-	check(KILOMETRAGE >= 1),
-);
 
--- ============================================================
--- ============================================================
---                      ALTER TABLE
--- ============================================================
--- ============================================================
 
+alter table POSITIONS
+      add constraint fk1_position foreign key (NUMERO_PARKING)
+      	  references PARKINGS (NUMERO_PARKING);
 
 alter table PARKINGS
       add constraint fk1_parking foreign key (CODE_POSTAL)
       	  references COMMUNES (CODE_POSTAL);
 
-alter table PLACES
-      add constraint fk1_place foreign key (NUMERO_PARKING)
-      	  references PARKINGS (NUMERO_PARKING);
-
 alter table STATIONNEMENTS
       add constraint fk1_stationnement foreign key (NUMERO_PLACE)
-      	  references PLACES (NUMERO_PLACE);
+      	  references POSITIONS (NUMERO_PLACE);
 	  
 alter table STATIONNEMENTS
       add constraint fk2_stationnement foreign key (NUMERO_IMMATRICULATION)
       	  references VEHICULES (NUMERO_IMMATRICULATION);
+
 
