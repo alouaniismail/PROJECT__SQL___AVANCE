d147e142296d05272d93b753301a6fe5247efbf0
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-20 12:05:20 +0100

    version 3.3.3

--- aff_vues_c.sql
+++ aff_vues_c.sql
@@ -13,14 +13,8 @@
 --    Données utiles pour les affichages suivants
 -- ============================================================
 
-insert  into  STATIONNEMENTS VALUES (31, '2022-10-03 11:10:58', '2022-10-03 12:55:53', 30, 'LP73BN');
-insert  into  STATIONNEMENTS VALUES (32, '2022-10-03 12:56:58', '2022-10-03 13:55:53', 1, 'LP73BN');
-insert  into  STATIONNEMENTS VALUES (33, '2022-10-03 11:10:58', '2022-10-03 12:55:53', 50, 'LP73BX');
-insert  into  STATIONNEMENTS VALUES (34, '2022-10-03 12:56:58', '2022-10-03 13:55:53', 80, 'LP73BX');
-insert  into  STATIONNEMENTS VALUES (35, '2022-10-03 11:10:58', '2022-10-03 12:55:53', 90, 'LP73BO');
-insert  into  STATIONNEMENTS VALUES (36, '2022-10-03 12:56:58', '2022-10-03 13:55:53', 107, 'LP73BO');
 
-
+insert  into  STATIONNEMENTS VALUES (31, '2022-07-23 13:23:17', '2022-07-23 15:13:59', 367, 'KV02MI');
 
 -- ============================================================
 --                vue_voitures__deux_parkings_h
