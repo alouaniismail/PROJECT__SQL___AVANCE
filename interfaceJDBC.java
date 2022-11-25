fa5f9b3fc37e7bc5e6b361801e4b06a4a38d5790
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-26 00:33:06 +0100

    version 6.3.0(correction legere)2/2 .//()...

--- interfaceJDBC.java
+++ interfaceJDBC.java
@@ -151,14 +151,14 @@ insertions2();
 	   	Scanner scanner15=new Scanner(System.in);
 		String str1=scanner15.nextLine();
      	SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD HH24:MI:SS");
-	java.sql.Date date1 = format.parse(str1);
+	Date date1 = format.parse(str1);
 
 		
 		System.out.println("Entrez l'horaire de sortie");
 		Scanner scanner16=new Scanner(System.in);
 		String str2=scanner16.nextLine();
         	SimpleDateFormat format2 = new SimpleDateFormat("YYYY-MM-DD HH24:MI:SS");
-	java.sql.Date date2=format2.parse(str2);
+	Date date2=format2.parse(str2);
 
 			System.out.println("Entrez le numéro de la place");
 		Scanner scanner17=new Scanner(System.in);
