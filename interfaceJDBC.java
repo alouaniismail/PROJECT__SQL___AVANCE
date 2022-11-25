c66abad5e6ff02f4e9ad319d4865c30be860d3a7
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-11-26 00:07:23 +0100

    version 6.3.0

--- interfaceJDBC.java
+++ interfaceJDBC.java
@@ -9,6 +9,7 @@ public static void main(String[] args) throws SQLException, ClassNotFoundExcepti
     Scanner scanner100=new Scanner(System.in);
     while(scanner100.nextInt()!=0){
 insertions1();
+insertions2();
     }
 }
     public static void insertions1()
@@ -91,7 +92,28 @@ insertions1();
 
      public static void insertions2()
 	 throws SQLException, ClassNotFoundException, java.io.IOException{
+	 	OracleDataSource ods=new OracleDataSource();
+        ods.setUser("ialouani"); 
+        ods.setPassword("ialouani");
+	ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
 	  System.out.println("Entrez le numero de la place:");
+	  	Scanner scanner13=new Scanner(System.in);
+	System.out.println("Veuillez choisir parmi le menu suivant");
+	System.out.println("1.Insertions(incluant des mises à jou)");
+	System.out.println("2.Consultations(incluant les vues de statistiques)");
+	int choix=scanner13.nextInt();
+	if(choix==1){
+	    System.out.println("Veuillez suivre les instructions suivantes:");
+	System.out.println("1.Communes");
+	System.out.println("2.Parkings");
+	System.out.println("3.Positions");
+	System.out.println("4.Stationnement");
+	System.out.println("5.Vehicules");
+	Scanner scanner19 =new Scanner(System.in);
+	Connection conn=null;
+	PreparedStatement stmt=null;
+	if(scanner19.nextInt()==3){
+	
 		Scanner scanner10=new Scanner(System.in);
 		int seven=scanner10.nextInt();
 		
@@ -107,11 +129,46 @@ insertions1();
 					   +" (NUMERO_PLACE, NOM_PLACE, NUMERO_PARKING)"
                                               + " values (?,?,?)");
 	    
-		stmt.setInt(10,seven);
-		stmt.setString(11,eight);
-		stmt.setInt(12,nine);
+		stmt.setInt(1,seven);
+		stmt.setString(2,eight);
+		stmt.setInt(3,nine);
 		stmt.executeUpdate();
+	}
+	else{
+	    System.out.println("Entrez un id de stationnement");
+	Scanner scanner14=new Scanner(System.in);
+		int ten=scanner14.nextInt();
+		
+		System.out.println("Entrez la date de stationnement");
+		Scanner scanner15=new Scanner(System.in);
+		String eleven=scanner15.nextDate();
+		
+		System.out.println("Entrez l'horaire de sortie");
+		Scanner scanner16=new Scanner(System.in);
+		String tweleve=scanner12.nextDate();
 
+			System.out.println("Entrez le numéro de la place");
+		Scanner scanner17=new Scanner(System.in);
+		String thirteen=scanner17.nextInt();
+			System.out.println("Entrez le num d'immatriculation");
+		Scanner scanner18=new Scanner(System.in);
+		String fourteen=scanner18.nextDate();
+
+		
+		conn=ods.getConnection();
+		stmt=conn.prepareStatement("insert into STATIONNEMENTS "
+					   +" (ID_STATIONNEMENT, DATE_STATIONNEMENT, HORAIRE_SORTIE,NUMERO_PLACE,NUMERO_IMMATRICULATION)"
+                                              + " values (?,?,?,?,?)");
+	    
+		stmt.setInt(1,ten);
+		stmt.setDate(2,eleven);
+		stmt.setDate(3,tweleve);
+		stmt.setInt(4,thirteen);
+		stmt.setInt(5,fourteen);
+		stmt.executeUpdate();
+	}
+	}
      }
 }
+
 
